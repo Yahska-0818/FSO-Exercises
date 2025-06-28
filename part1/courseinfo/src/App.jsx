1a80f3ba94143162ae7e642a9694774258cf30a2
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-06-28 02:07:34 +0000

    fixed 1.1 now done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,13 +1,4 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const Header = (props) => {
+const Header = (props) => {
     return(
       <h1>{props.course}</h1>
     )
@@ -34,6 +25,16 @@ const App = () => {
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     )
   }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
 
   return (
     <div>
