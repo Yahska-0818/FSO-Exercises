82779d24c3b9e075214425160738ef2af3493169
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-06-28 10:30:24 +0000

    1.8 done as statistics is already a separate component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -26,7 +26,6 @@ const DisplayStats = ({good,neutral,bad}) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
