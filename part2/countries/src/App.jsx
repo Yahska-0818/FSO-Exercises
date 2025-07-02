7ab765c54b14f67acddf3a3af704f9959712308f
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-02 12:52:10 +0000

    2.19 done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,7 +3,6 @@ import CountryList from './components/CountryList'
 import axios from "axios"
 
 const App = () => {
-  const [country,setCountry] = useState("")
   const [countryValue,setCountryValue] = useState('')
   const [allCountries,setAllCountries] = useState([])
   
@@ -11,12 +10,14 @@ const App = () => {
     axios
         .get("https://studies.cs.helsinki.fi/restcountries/api/all")
         .then(response=>{
-          const simplifiedCountries = response.data.map(country => ({
+          const simplifiedCountries = response.data.map((country,index) => ({
             name: country.name.common,
             capital: country.capital ? country.capital[0] : "N/A",
             area: country.area,
             languages: country.languages ? Object.values(country.languages) : "N/A",
-            flag: country.flags ? Object.values(country.flags)[0] : "N/A"
+            flag: country.flags ? Object.values(country.flags)[0] : "N/A",
+            show: false,
+            id: index
           }))
           setAllCountries(simplifiedCountries)
         })
@@ -35,20 +36,12 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-    if (showCountries.length === 1 && showCountries[0].name !== "Too many matches, specify another filter") {
-      setCountry(showCountries[0].name)
-    } else {
-      setCountry("")
-    }
-  }, [showCountries])
-
   return(
     <div style={{display:'flex',flexDirection:'column',gap:"1rem",margin:"1rem",fontFamily:"Calibri"}}>
       <form style={{display:'flex',gap:"1rem",alignItems:"center",fontSize:"1.5rem"}}>
         Country: <input type="text" value={countryValue} onChange={handleChange} style={{fontSize:"1.3rem",fontFamily:"Calibri"}}/>
       </form>
-      <CountryList countries={showCountries} countryValue={countryValue.toUpperCase()}/>
+      <CountryList countries={showCountries} countryValue={countryValue.toUpperCase()} allCountries={allCountries} changeCountries={setAllCountries}/>
     </div>
   )
 }
--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,4 +1,11 @@
-const CountryList = ({countries,countryValue}) => {
+const CountryList = ({countries,countryValue,allCountries,changeCountries}) => {
+
+    const toggleView = (id) => {
+        let currentCountries = [...allCountries]
+        currentCountries[id].show = true
+        changeCountries(currentCountries)
+    }
+
     if (countryValue == "INDIA" || countryValue == "SUDAN" || countryValue == "SOUTH AFRICA") {
         const countryList = countries.filter(country => country.name.toUpperCase() === countryValue)
         return (
@@ -25,15 +32,36 @@ const CountryList = ({countries,countryValue}) => {
     else if (countries.length > 1) {
         return(
         <ul style={{display:"flex",flexDirection:"column",gap:"1rem",padding:"0"}}>
-            {countries.map((country,index) =>
+            {countries.map((country,index) => country.show?
                 <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
+                        <h3>{country.name}</h3>
+                        <div>
+                            <p>Capital {country.capital}</p>
+                            <p>Area {country.area}</p>
+                        </div>
+                        <h3>Languages</h3>
+                        <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
+                            {country.languages ? country.languages.map((language,languageIndex)=>
+                                <li key={languageIndex}>{language}</li>
+                            ): "N/A"}
+                        </ul>
+                        <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
+                </li>
+                :
+                <li key={index} style={{fontSize:"1.5rem", listStyle:"none",display:"flex",gap:"1rem",alignItems:"center"}}>
                     <h3>{country.name}</h3>
+                    <button type="button" onClick={()=>toggleView(country.id)} style={{height:"3rem",width:"6rem",fontSize:"1.3rem"}}>Show</button>
                 </li>
             )}
         </ul>
     )
     }
-    else {
+    else if (countries.length === 1){
+        if (countries[0].name == "Too many matches, specify another filter") {
+            return (
+                <h1>Too many matches, specify another filter</h1>
+            )
+        }
         return(
             <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
                 {countries.map((country,index) =>
@@ -49,7 +77,7 @@ const CountryList = ({countries,countryValue}) => {
                                 <li key={languageIndex}>{language}</li>
                             ): "N/A"}
                         </ul>
-                        <img src={country.flag} style={{marginTop:"10px"}}/>
+                        <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
                     </li>
                 )}
             </ul>
