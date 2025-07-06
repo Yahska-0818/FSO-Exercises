aeddca822894eb58d112102d6168f24e8eeb95af
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-06 07:11:16 +0000

    Added loading to weather for countries and added back toggle view

--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -44,33 +44,64 @@ const CountryList = ({countries,countryValue,allCountries,changeCountries}) => {
         fetchWeather();
     }, [capital]);
 
+    const toggleView = (id) => {
+        let currentCountries = [...allCountries]
+        currentCountries[id].show = true
+        changeCountries(currentCountries)
+    }
+
 
     if (countryValue == "INDIA" || countryValue == "SUDAN" || countryValue == "SOUTH AFRICA") {
         const countryList = countries.filter(country => country.name.toUpperCase() === countryValue)
-        return (
-            <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
-                {countryList.map((country,index) =>
-                    <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
-                        <h3>{country.name}</h3>
-                        <div>
-                            <p>Capital: {country.capital}</p>
-                            <p>Area: {country.area}</p>
-                        </div>
-                        <h3>Languages</h3>
-                        <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
-                            {country.languages ? country.languages.map((language,languageIndex)=>
-                                <li key={languageIndex}>{language}</li>
-                            ): "N/A"}
-                        </ul>
-                        <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
-                        <h3>Weather in {country.capital}</h3>
-                        <p>Temperature {weather.temp} Celsius</p>
-                        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" srcset="" />
-                        <p>Wind {weather.wind} m/s</p>
-                    </li>
-                )}
-            </ul>
-        )
+        if (weather != null) {
+            return (
+                <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
+                    {countryList.map((country,index) =>
+                        <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
+                            <h3>{country.name}</h3>
+                            <div>
+                                <p>Capital: {country.capital}</p>
+                                <p>Area: {country.area}</p>
+                            </div>
+                            <h3>Languages</h3>
+                            <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
+                                {country.languages ? country.languages.map((language,languageIndex)=>
+                                    <li key={languageIndex}>{language}</li>
+                                ): "N/A"}
+                            </ul>
+                            <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
+                            <h3>Weather in {country.capital}</h3>
+                            <p>Temperature {weather.temp} Celsius</p>
+                            <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" srcset="" />
+                            <p>Wind {weather.wind} m/s</p>
+                        </li>
+                    )}
+                </ul>
+            )
+        } else {
+            return (
+                <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
+                    {countryList.map((country,index) =>
+                        <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
+                            <h3>{country.name}</h3>
+                            <div>
+                                <p>Capital: {country.capital}</p>
+                                <p>Area: {country.area}</p>
+                            </div>
+                            <h3>Languages</h3>
+                            <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
+                                {country.languages ? country.languages.map((language,languageIndex)=>
+                                    <li key={languageIndex}>{language}</li>
+                                ): "N/A"}
+                            </ul>
+                            <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
+                            <h3>Weather in {country.capital}</h3>
+                            <p>loading...</p>
+                        </li>
+                    )}
+                </ul>
+            )
+        }
     }
     else if (countries.length > 1) {
         return(
@@ -105,30 +136,55 @@ const CountryList = ({countries,countryValue,allCountries,changeCountries}) => {
                 <h1>Too many matches, specify another filter</h1>
             )
         }
-        return(
-            <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
-                {countries.map((country,index) =>
-                    <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
-                        <h3>{country.name}</h3>
-                        <div>
-                            <p>Capital: {country.capital}</p>
-                            <p>Area: {country.area}</p>
-                        </div>
-                        <h3>Languages</h3>
-                        <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
-                            {country.languages ? country.languages.map((language,languageIndex)=>
-                                <li key={languageIndex}>{language}</li>
-                            ): "N/A"}
-                        </ul>
-                        <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
-                        <h3>Weather in {country.capital}</h3>
-                        <p>Temperature {weather.temp} Celsius</p>
-                        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" srcset="" />
-                        <p>Wind {weather.wind} m/s</p>
-                    </li>
-                )}
-            </ul>
-        )
+        if (weather != null) {
+            return (
+                <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
+                    {countries.map((country,index) =>
+                        <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
+                            <h3>{country.name}</h3>
+                            <div>
+                                <p>Capital: {country.capital}</p>
+                                <p>Area: {country.area}</p>
+                            </div>
+                            <h3>Languages</h3>
+                            <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
+                                {country.languages ? country.languages.map((language,languageIndex)=>
+                                    <li key={languageIndex}>{language}</li>
+                                ): "N/A"}
+                            </ul>
+                            <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
+                            <h3>Weather in {country.capital}</h3>
+                            <p>Temperature {weather.temp} Celsius</p>
+                            <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" srcset="" />
+                            <p>Wind {weather.wind} m/s</p>
+                        </li>
+                    )}
+                </ul>
+            )
+        } else {
+            return (
+                <ul style={{display:"flex",flexDirection:"column",gap:"1.5rem",padding:"0"}}>
+                    {countries.map((country,index) =>
+                        <li key={index} style={{fontSize:"1.5rem", listStyle:"none"}}>
+                            <h3>{country.name}</h3>
+                            <div>
+                                <p>Capital: {country.capital}</p>
+                                <p>Area: {country.area}</p>
+                            </div>
+                            <h3>Languages</h3>
+                            <ul style={{display:"flex",flexDirection:"column",gap:"1rem"}} >
+                                {country.languages ? country.languages.map((language,languageIndex)=>
+                                    <li key={languageIndex}>{language}</li>
+                                ): "N/A"}
+                            </ul>
+                            <img src={country.flag} style={{marginTop:"10px",border:"solid 2px black"}}/>
+                            <h3>Weather in {country.capital}</h3>
+                            <p>loading...</p>
+                        </li>
+                    )}
+                </ul>
+            )
+        }
     }
 }
 
