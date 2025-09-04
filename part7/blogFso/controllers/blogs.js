89c66d542bc34e8327f5dee669317f7010d3390c
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-09-04 04:45:54 +0000

    done till 9.17

--- part7/blogFso/controllers/blogs.js
+++ part7/blogFso/controllers/blogs.js
@@ -7,6 +7,11 @@ blogsRouter.get('/', async (request, response) => {
   response.json((blogs))
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id).populate('user', { username: 1, name: 1 , id: 1})
+  response.json(blog)
+})
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
